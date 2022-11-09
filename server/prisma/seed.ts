7ea1ca5131f3943d7b1cdcc77e67aa52416bae34
Wanderlei Pereira <wanderlei2583@gmail.com>
2022-11-09 13:06:51 -0300

    chore: alter server

---
+++ server/prisma/seed.ts
@@ -0,0 +1,63 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+async function main(){
+
+  const user = await prisma.user.create({
+    data: {
+      name: 'Wander',
+      email: 'wanderlei@gmail.com',
+      avatarUrl: 'https://github.com/wander2583.png',
+    }
+  })
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Exemple Pool',
+      code: 'BOLAO123',
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id
+        }
+      }
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-09T08:00:00.953Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR',
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-10T08:00:00.953Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+
+      guesses: {
+        create: {
+          firsTeamPoints: 2,
+          secondTeamPoints: 1,
+
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+}
+
+main()
